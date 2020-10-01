00cfaa38f2b53ece48350282be1a937dc4df2b9a
tonystark2005 <sanketsacharya@gmail.com>
2020-10-01 16:00:20 -0700

    initial changes

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 ENVIRONMENT="$1"
 AWS_PROFILE="$2"
-[[ $ENVIRONMENT = 'prod' ]] && ENV_FILE=.env.production || ENV_FILE=.env.development
+#[[ $ENVIRONMENT = 'prod' ]] && ENV_FILE=.env.production || ENV_FILE=.env.development
+if [ $ENVIRONMENT = 'prod' ]; then
+	ENV_FILE=.env.production
+else
+	ENV_FILE=.env.development
+fi
 export $(grep -v '^#' $ENV_FILE | xargs)
 DOMAIN_NAME_LENGTH=$(echo "$GATSBY_EARTHBUCKET_HOSTNAME" | awk '{n=split($0, array, ".")} END{print n }')
 if [ $DOMAIN_NAME_LENGTH = 3 ]; then
@@ -9,7 +14,8 @@ if [ $DOMAIN_NAME_LENGTH = 3 ]; then
 else
 	DOMAIN_NAME=$(echo $GATSBY_EARTHBUCKET_HOSTNAME | awk -F. '{print $1 "." $2}')
 fi
-NAMESPACE=$(sed -e "s,\.,-," <<< $DOMAIN_NAME)
+#NAMESPACE=$(sed -e "s,\.,-," <<< $DOMAIN_NAME)
+
 aws s3 cp ./scripts/earthbucket-blog-post-seed.png s3://"$AWS_EARTHBUCKET_MEDIA_BUCKET"/blog/posts/images/ --profile $AWS_PROFILE
 DISTRIBUTION_ID=$(aws cloudformation list-exports --query "Exports[?Name=='${NAMESPACE}-${ENVIRONMENT}-AirCdnDistributionId'].Value" --output text --profile $AWS_PROFILE)
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths '/*' --profile $AWS_PROFILE
